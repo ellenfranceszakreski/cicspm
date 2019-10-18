e0b0094c8b7bc3976893c20d24e9c9ce2de0a999
ellenfranceszakreski <efzakreski@mac.com>
2019-10-18 14:30:26 -0400

    Update DartelCreateTemplate_remake_cicjobs.sh

--- DartelCreateTemplate_remake_cicjobs.sh
+++ DartelCreateTemplate_remake_cicjobs.sh
@@ -1,1 +1,46 @@
 #!/bin/sh
+# ./DartelCreateTemplate_remake_cicjobs.sh
+
+# analysis directory
+AnalysisDir=/data/scratch/zakell/vbm #<- make sure this is correct
+JobName="DartelCreateTemplate"
+
+JobDir=$AnalysisDir/cicjobs/$JobName # cic job .m files kept here
+
+# (re)make Job directory
+test -d $JobDir && rm -r $JobDir
+mkdir $JobDir
+ # get template
+TemplateFile=$AnalysisDir/Scripts/DartelCreateTemplate_job_template.m
+if [ ! -f $TemplateFile ]; then
+  printf "error: could not find TemplateFile at %s\n" $TemplateFile
+  exit 1
+fi
+
+cicjoblistFile=$JobDir/cicjoblist
+touch $cicjoblistFile
+
+## make each jobs
+JobFile=$JobDir/DartelCreateTemplate_rc1.txt
+touch $JobFile
+echo "channel_prefixes = 'rc1';" > $JobFile
+cat $TemplateFile >> $JobFile
+echo "matlab -nodisplay -nodesktop -nosplash -r \"run('"$JobFile"')\"" >> $cicjoblistFile
+unset JobFile
+
+JobFile=$JobDir/DartelCreateTemplate_rc2.txt
+touch $JobFile
+echo "channel_prefixes = 'rc2';" > $JobFile
+cat $TemplateFile >> $JobFile
+echo "matlab -nodisplay -nodesktop -nosplash -r \"run('"$JobFile"')\"" >> $cicjoblistFile
+unset JobFile
+
+JobFile=$JobDir/DartelCreateTemplate_rc1_rc2.txt
+touch $JobFile
+echo "channel_prefixes = {'rc1', 'rc2'};" > $JobFile
+echo "matlab -nodisplay -nodesktop -nosplash -r \"run('"$JobFile"')\"" >> $cicjoblistFile
+cat $TemplateFile >> $JobFile
+
+
+
+
