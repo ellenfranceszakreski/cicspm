bdd09a543d28107517c0c5c5e756b61267ffadf6
ellenfranceszakreski <efzakreski@mac.com>
2019-10-18 14:13:45 -0400

    Create DartelCreateTemplate_job_template.m

---
+++ DartelCreateTemplate_job_template.m
@@ -0,0 +1,22 @@
+% append this code after defining variable channel_prefixes
+% e.g. channel_prefixes = {'rc1'}; channel_prefixes = {'rc1','rc2'};
+
+
+% add this code to jobs using spm for the cluster.
+AnalysisDir='/data/scratch/zakell/vbm'; % <-make sure this is correct!
+%% set up cluster
+number_of_cores=12;
+d=tempname();% get temporary directory location
+mkdir(d);
+% create cluster
+cluster = parallel.cluster.Local('JobStorageLocation',d,'NumWorkers',number_of_cores);
+matlabpool(cluster, number_of_cores);
+%% run analysis
+% get data for subject
+addpath([AnalysisDir,'/Scripts']);
+addpath(genpath([spm('dir'),'/config']));
+
+matlabbatch = matlabbatch_DartelCreateTemplate(channel_prefixes);
+
+spm('defaults','PET'); % defaults for VBM
+spm_jobman('run', matlabbatch);
